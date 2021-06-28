67a3bae93551974b4d6090f30646ff0fb8e137a3
stackedwaffles88 <tripcmom@gmail.com>
2021-06-28 14:36:27 -0400

    Add project files.

---
+++ MyNotesApp/MyNotesApp/AddNotePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyNotesApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddNotePage : ContentPage
+    {
+        public AddNotePage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
